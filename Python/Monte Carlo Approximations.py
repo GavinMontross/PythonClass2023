92fb74f972366875d3508c072edae36d58d181dd
GSM429 <gavmontross@gmail.com>
2023-11-07 15:12:55 -0500

    pigandmontecarlo

--- Python/Monte Carlo Approximations.py
+++ Python/Monte Carlo Approximations.py
@@ -1,4 +1,5 @@
 import random
+"""
 def sevenCardGame():
     cards = ["b"] * 5 + ["r"] *2
     random.shuffle(cards)
@@ -16,3 +17,65 @@ for _ in range(TRIALS):
     if result:
         wins += 1
 print(wins/TRIALS)
+"""
+
+
+"""
+def rollDf():
+    faces = ['+', '+', '_','_', '-', '-']
+    roll = random.choice(faces)
+    return roll
+
+def sim4df():
+    total = 0
+    for _ in range(4):
+        result = rollDf()
+        if result == '+':
+            total += 1
+        elif result == '-':
+            total -= 1
+    return total
+
+TRIALS = 100000
+higherThanZero = 0
+for _ in range(TRIALS):
+    roll = sim4df()
+    if roll >= 0:
+        higherThanZero += 1
+print(higherThanZero/TRIALS)
+"""
+
+
+def coinFlip():
+    coin = ['H','T']
+    flip = random.choice(coin)
+    return flip
+
+player1 = "THH"
+player2 = "HHT"
+
+def playGame():
+    result = ''
+    while True:
+        result += coinFlip()
+        if player1 in result:
+            return 'player1win'
+            break
+        elif player2 in result:
+            return 'player2win'
+            break
+        else:
+            continue
+
+
+player1total = 0
+player2total = 0
+TRIALS = 100000
+for _ in range(TRIALS):
+    result = playGame()
+    if result == 'player1win':
+        player1total += 1
+    elif result == 'player2win':
+        player2total += 1
+print("player1's win %:", player1total/TRIALS)
+print("player2's win %:", player2total/TRIALS)
--- Python/Pig Game/pig.py
+++ Python/Pig Game/pig.py
@@ -4,9 +4,9 @@ def rolld6():
     return roll
 
 
-def holdat20():
+def holdat20(limit=20):
     turntotal = 0
-    while turntotal < 20:
+    while turntotal < limit:
         roll = rolld6()
         if roll != 1:
             turntotal += roll
@@ -17,8 +17,6 @@ def holdat20():
             break
     #print("Turn total:",turntotal)
     return turntotal
-holdat20()
-
 
 def holdAt20Outcomes(trials):
     results = {0:0,20:0,21:0,22:0,23:0,24:0,25:0}
@@ -29,8 +27,39 @@ def holdAt20Outcomes(trials):
         results[score] = results[score]/trials
     return results
 
+def holdAt20orGoal(limit = 20, score = 0):
+    turnTotal = 0
+    while turnTotal < limit:
+        roll = random.randrange(1,6)
+        print("Roll:", roll)
+        if roll == 1:
+            turnTotal = 0
+            break
+        else:
+            turnTotal += roll
+    newScore = score + turnTotal
+    print("New score: ", newScore)
+    
+    
+    return newScore
 
-results = holdAt20Outcomes(100000)
-print("Score\tEstimated Probability")
-for score in results:
-    print(score,results[score],sep="\t")
+print(holdAt20orGoal(20,90))
+
+#limit = int(input("What is the limit for the hold at x game?: "))
+#trials = int(input("How many trials?: "))
+
+def holdAtXOutcomes(limit, trials):
+    results = {0:0}
+    for score in range(limit, limit + 6):
+        results[score] = 0
+    for _ in range(trials):
+        turnTotal = holdat20(limit=limit)
+        score = holdat20(limit)
+        results[turnTotal] += 1
+    return results
+
+
+#results = holdAtXOutcomes(limit, trials)
+#print("Score\tEstimated Probability")
+#for score in results:
+#    print(score, results[score]/trials,sep = '\t')
