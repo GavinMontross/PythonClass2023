258e93efb81da40e879c8b3d59ecd596f0ed8d37
GSM429 <gavmontross@gmail.com>
2023-11-29 10:04:51 -0500

    regex

--- Regex Assignment/RegexHW.py
+++ Regex Assignment/RegexHW.py
@@ -6,8 +6,17 @@ text = file.read()
 #Question 1
 pattern = "\w*cat\w*|\w*dog\w*"
 answer = re.findall(pattern, text)
-print (len(answer))
+#print (len(answer))
 
 #Question 2
+pattern = "\\b\\w{4}\\b"
+answer = re.findall(pattern, text)
+#print(len(answer))
 
+#Question 3
+pattern = "\w*hun\w*"
+answer = re.findall(pattern, text)
+#print (len(answer))
 
+#Question 4
+ingPattern = 
